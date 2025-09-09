0ff1ad1a06fad1c80bef90809c1d438c0e2dbbfa
SpiralGang <Spiralgang@outlook.com>
2025-09-09 04:40:57 -0600

    Update and rename .github/workflows/run.yml to actions/runner/linux/arm64/run.sh

--- actions/runner/linux/arm64/run.sh
+++ actions/runner/linux/arm64/run.sh
@@ -1,6 +1,4 @@
 # Create a folder
-$ mkdir actions-runner && cd actions-runner
-
 # Download the latest runner package
 $ curl -o actions-runner-linux-arm64-2.328.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-arm64-2.328.0.tar.gz
 
